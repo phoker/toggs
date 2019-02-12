df9664816645b18bba6ddc61128627c66635e68d
phoker <jinful.dodger@gmail.com>
2019-02-11 20:41:26 -0800

    readme updated, fixture reverted

--- src/__fixtures__/toggs.fixture.js
+++ src/__fixtures__/toggs.fixture.js
@@ -3,22 +3,11 @@ import Toggs from './../index';
 
 const Example = () => {
   const [ checked, setChecked ] = useState(false)
-  const [ width, setWidth ] = useState(0)
   return (
     <>
       <Toggs
         checked={checked}
-        width={parseInt(width, 10)}
         onChange={() => { setChecked(!checked) }}
-        theme="ios"
-      />
-      <input
-        type="range"
-        min="0"
-        max="100"
-        value={width}
-        step="10"
-        onChange={({ target: { value }}) => { setWidth(value) }}
       />
     </>
   )
